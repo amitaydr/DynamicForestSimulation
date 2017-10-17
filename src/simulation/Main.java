c9c44d5644e54e58b2d2b8aa1a991dbf264fce9e
amitaydr <amitaydr@gmail.com>
2017-10-17 18:02:55 +0300

    log dir created if doesnt exist

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -1,6 +1,7 @@
 package simulation;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -11,10 +12,10 @@ import visualization.Plotter;
 import dynamicGraph.Forest;
 
 public class Main {
-	private static int size = 6;
+	private static int size = 7;
 	private static int C = 2;
-	private static int chunkSize = 6;
-	private static long  iterations = 10000000L;
+	private static int chunkSize = 14;
+	private static long  iterations = 50000000L;
 
 	
 	private static LinkedList<String> smallLogger;
@@ -22,14 +23,14 @@ public class Main {
 	private static int smallLoggerSize;
 	static FileWriter fw = null;
 	static BufferedWriter bw = null;
-	public static String logdir = "C:\\Users\\amitaydr\\Desktop\\ProjectRepo\\DynamicForestSimulation\\logs\\newSession3\\";
+	public static String logdir = "C:\\logs\\"; //"C:\\Users\\amitaydr\\Desktop\\ProjectRepo\\DynamicForestSimulation\\logs\\session5\\";
 	private static boolean doLogAll = false;
-	private static boolean animateResult = false;
+	private static boolean animateResult = true;
 	private static int bestScore;
 
 	
 	private static void initLog(){	
-		
+		new File(logdir).mkdir();
 		if (doLogAll) {
 			String fileName = logdir
 					+ LocalDateTime.now().format(
@@ -181,16 +182,32 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		//running a few simulations in a row
+//		long[] itrs = new long[]{10000000L,50000000L,100000000L,500000000L,1000000000};
+//		for(int i = 7;i<21;i++){
+//			for(long itr : itrs) {
+//				size = i;
+//				chunkSize = 2*size;
+//				iterations = itr;
+//				mainSimulation();
+//				cleanAll();
+//			}
+//		}
 		
-		for(int i = 0;i<15;i++){
-			size = 6 + i;
-			chunkSize = 2*size;
-			mainSimulation();
-			iterations *= Math.pow(10.0, 1/7.0);
-			cleanAll();
-		}
-//		mainSimulation();
-//		Plotter.plotFromLogFile("bestChunk11_10_2017_16_42");
+		//running a few simulations in a row
+//		for(int i = 0;i<15;i++){
+//			size = 6 + i;
+//			chunkSize = 2*size;
+//			mainSimulation();
+//			iterations *= Math.pow(10.0, 1/7.0);
+//			cleanAll();
+//		}
+		
+		
+		mainSimulation();
+		
+		//in order to run a visualization of an existing log uncomment the next line and switch file name
+//		Plotter.plotFromLogFile("N=6_C=2_ChunckSize=12_Iterations=1000000000Time=17_10_2017_09_14");
 	}
 
 
